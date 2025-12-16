7993937ccd41334731477f8e32e4a05763ef80ed
Trie <thanhtriet1007@gmail.com>
2025-12-16 08:23:07 +0700

    training161225

---
+++ training161225/780C.cpp
@@ -0,0 +1,37 @@
+/* Author : Triet Do Thanh - FPT University */
+
+#include <bits/stdc++.h>
+
+#define int            long long
+#define fu(i, l, r)    for (int i = (l); i <= (r); ++i)
+#define fd(i, r, l)    for (int i = (r); i >= (l); --i)
+
+using namespace        std;
+
+typedef pair<int, int> ii;
+
+const int       N      = 1e6 + 7;
+const long long INF     = 1e18 + 7;
+const long long MOD    = 1e9 + 7;
+
+void solve() {
+    //Trie's solution here
+    //i luv BN
+        
+}
+
+#define TASK "test"
+
+signed main()
+{
+   ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+   if (fopen("input.txt", "r")) {
+      freopen("input.txt", "r", stdin);
+      freopen("output.txt", "w", stdout);
+   }
+   int t = 1;
+   //cin >> t;
+   while (t--)
+   solve();
+   return 0;
+}
